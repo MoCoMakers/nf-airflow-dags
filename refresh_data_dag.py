49bce83854989c0a21758ed15f428fa0163e11a5
Emine Oymak <e_oymak@acs.org>
2025-03-03 17:37:21 +0300

    Data refresh DAG

--- refresh_data_dag.py
+++ refresh_data_dag.py
@@ -5,7 +5,6 @@ from airflow.hooks.postgres_hook import PostgresHook
 import traceback
 from pathlib import Path
 from datetime import datetime, timedelta
-import csv
 import pandas as pd
 
 
@@ -17,13 +16,13 @@ r2 FLOAT, auc FLOAT, ec50 FLOAT, ic50 FLOAT,
 name VARCHAR(255), moa VARCHAR(1000), target VARCHAR(1000),
 disease_area VARCHAR(1000), indication VARCHAR(1000),
 smiles VARCHAR(1500), phase VARCHAR(255), passed_str_profiling boolean, row_name VARCHAR(255),
-CONSTRAINT sprime_dose_curve_pk PRIMARY KEY (broad_id, depmap_id))"""
+CONSTRAINT sprime_dose_curve_pk PRIMARY KEY (broad_id, depmap_id, screen_id))"""
 
 secondary_dose_curve_table_insert_sql = """INSERT INTO im_dep_sprime_secondary_dose_curve 
 (broad_id, depmap_id, ccle_name, screen_id, upper_limit, lower_limit, 
 slope, r2, auc, ec50, ic50, name, moa, target, disease_area, indication,
 smiles, phase, passed_str_profiling, row_name) 
-VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT(broad_id, depmap_id) DO NOTHING"""
+VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT(broad_id, depmap_id, screen_id) DO NOTHING"""
 
 omics_mutations_matrix_table_sql = """CREATE TABLE IF NOT EXISTS im_dep_sprime_damaging_mutations (cell_line VARCHAR(255), gene VARCHAR(255), value INTEGER)"""
 
@@ -31,6 +30,8 @@ omics_mutations_matrix_table_insert_sql = "INSERT INTO im_dep_sprime_damaging_mu
 
 DEP_PRISM_PATH = "/home/gatlay/nf_streamlit/app/data/DepMap/Prism19Q4"
 DEP_PUBLIC_PATH = "/home/gatlay/nf_streamlit/app/data/DepMap/Public24Q2"
+
+SEC_RESP_DOSE_CURVE = "secondary-screen-dose-response-curve-parameters.csv"
 
 
 # Define the default_args dictionary
@@ -48,33 +49,42 @@ dag = DAG(
 )
 
 def refreshData():
-    print("Data refresh process started.")
-    #refresh_data(DEP_PRISM_PATH, "secondary-screen-dose-small-set.csv") 
-    refresh_data(DEP_PRISM_PATH, "secondary-screen-dose-response-curve-parameters.csv") 
-    print("Data refresh process ended.")         
+    refresh_data("SEC_RESP_DOSE_CURVE") 
+             
 
-def refresh_data(file_path, file_name):
+def refresh_data(process_type):
+    start_time = datetime.now()
+    
+    table_create_sql = None
+    table_name = None
+    data_insert_sql = None
+    data_file_name = None
+    file_path = None
+    if process_type == "SEC_RESP_DOSE_CURVE":
+        table_name = "im_dep_sprime_secondary_dose_curve"
+        table_create_sql = secondary_dose_curve_table_sql
+        data_insert_sql = secondary_dose_curve_table_insert_sql
+        file_path = DEP_PRISM_PATH
     input_folder = Path(file_path)
     try:
+        print(f"Data refresh process started for {table_name}. Table will be created if it doesn't exist.")
         pg_hook = PostgresHook(postgres_conn_id='Comp_Bio_Hub_Postgres', schema='public')
         pg_conn = pg_hook.get_conn()
         cursor = pg_conn.cursor()
-        cursor.execute(secondary_dose_curve_table_sql)
+        cursor.execute(table_create_sql)
         pg_conn.commit()
-        print("A new table has been created.")
         
-        print(f"{file_name} is being processed.")
+        print(f"Started to load {data_file_name} content into memory.")
         # Read the CSV file
-        csv_data = pd.read_csv(input_folder / file_name)
+        csv_data = pd.read_csv(input_folder / data_file_name)
 
         rows = []
-        print("CSV content is being loaded to a list.")
         for val in csv_data.values:
             rows.append(tuple(list(val.flatten())))
         
-        print("Started to write the data list to DB.")
-        for rows_batch in batch(rows, 1000):
-            cursor.executemany(secondary_dose_curve_table_insert_sql, rows_batch)
+        print(f"Started inserting the data in {data_file_name} file into {table_name} table.")
+        for rows_batch in batch(rows, 20000):
+            cursor.executemany(data_insert_sql, rows_batch)
             pg_conn.commit()
             print(f"{len(rows_batch)} records done.") 
         
@@ -86,6 +96,9 @@ def refresh_data(file_path, file_name):
     finally:
         pg_conn.commit()
         cursor.close()
+        end_time = datetime.now()
+        print(f"Duration to complete the refresh process for {table_name}: {end_time - start_time}")
+
 
 def batch(iterable, n):
     l = len(iterable)
@@ -102,7 +115,7 @@ refresh_data_task = PythonOperator(
     task_id='refresh_data_task',
     python_callable=refreshData,
     dag=dag,
-    execution_timeout=timedelta(seconds=10000),
+    execution_timeout=timedelta(seconds=100000),
 )
 
 end = DummyOperator(
